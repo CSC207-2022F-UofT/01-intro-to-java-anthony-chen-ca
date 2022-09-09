0a34d1a1373aa1b9aaeb09be5e7d7300cf77304a
topazerino <47570422+topazerino@users.noreply.github.com>
2022-09-09 15:59:58 -0400

    lesgoo

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -44,6 +44,8 @@ public class Basics {
          *                Hello World!
          */
 
+        System.out.println("Hello World!");
+
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -63,7 +65,7 @@ public class Basics {
          *                value 100.
          */
 
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -99,6 +101,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: "+i);
+        }
 
 
     }
@@ -142,7 +148,11 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] arr = to_split.split(" ");
+
+        for (String word : arr) {
+            ret.append(word.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +180,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        for (int i = 1; i < arr.length; i = i + 2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
